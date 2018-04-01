ac73b64c833b4b73c6f1148579a15fde8fde3db3
Andreas Strauman <andreas@strauman.net>
2018-04-01 14:21:09 +0200

    Update

--- src/aftercompile.sh
+++ src/aftercompile.sh
@@ -1,15 +1,41 @@
-rm quickstart.zip
-zip quickstart.zip example.tex handin.sty img/universityTromso.pdf
-pattNoUsepack=
-perl -pe 's/^(?!\\usepackage).*//; s/^\\usepackage(?:\[[^\]]+\])?\{([^\{]+)\}/\1/;s/^\n//g' packages.tex > dependencies.txt
-perl -i -pe 's/\n/,/g' dependencies.txt
+cd $sourceDir;
+cp $tmpDir/handin.sty ./
 
-mv quickstart.zip ../
-rm handin.sty.bak
+
+# cp bin/example.pdf ./
+cp example.tex ./handin
+cp img/universityTromsoLogo.pdf ./handin
+
+cp layout.tex handin/
+
+cd handin/
+outHandle "Error in latexmk example" latexmk -pdf "example.tex" -outdir="./bin" --shell-escape -interaction=nonstopmode -f
+outHandle "Error in latexmk layout" latexmk -pdf "layout.tex" -outdir="./bin" --shell-escape -interaction=nonstopmode -f
+cp bin/example.pdf ./
+cp bin/layout.pdf ./
+rm -rf bin
+rm layout.tex
+cd ../
+# rm example.zip
+# zip example.zip example.tex example.pdf img/universityTromso.pdf
+
+# cp example.zip ../
+# cp example.zip handin/
+
 rm ../example.tex
+cp example.tex ../
+
 rm ../handin.sty
+cp handin.sty ../
+
 rm -rf ../img
 cp -r ./img ../
-cp example.tex ../
-cp handin.sty ../
+
+rm ../layout.pdf
 cp layout.pdf ../
+
+zip handinCTAN.zip -r handin/
+mv handinCTAN.zip ../
+rm -rf ../handin
+mv handin ../
+# unzip example.zip -d example
