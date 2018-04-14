ca8620c5f525306c145d29d0714c63e35bc0d0fb
Andreas Strauman <andreas@strauman.net>
2018-04-14 18:38:41 +0200

    BIG doc update

--- src/makedocs.sh
+++ src/makedocs.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-latexpand --keep-comments texpack.tex > tmpDocs/expanded.tex
+echo "Combining files"
+latexpand --keep-comments texpack.tex > tmpDocs/expanded.tex 2>./expand.log
+if [ ! "$?" -eq "0" ];then
+  echo "Something went wrong when combining files $exitCode"
+  cat ./expand.log
+  exit 1;
+fi
+rm ./expand.log
+echo "Parsing docs"
 cd tmpDocs
-perl ./docextract.pl -i expanded.tex -T './' --skeleton=docskeleton.tex -v > docout.tex
+perl ./lib/docextract.pl -i expanded.tex -T './lib' --skeleton=docskeleton.tex -v > docout.tex
+if [ ! "$?" -eq "0" ];then
+  echo "Something went wrong when parsing docs"
+  exit 1;
+fi
+echo "Cleaning up"
+rm expanded.tex
+echo "Docs are ready in handin-LaTeX/src/tmpDocs/docout.tex"
